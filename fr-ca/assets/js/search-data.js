09fe4bb04129ca6b8368755c8b10339b73a4f9c2
fransfela <randyrff@gmail.com>
2025-10-18 18:16:10 +0000

    Deploying to gh-pages from @ fransfela/fransfela.github.io@0aa658b8fab89621b247c71d2e5c6a5e91b35fbf 🚀

--- fr-ca/assets/js/search-data.js
+++ fr-ca/assets/js/search-data.js
@@ -50,7 +50,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae",
           section: "menu de navigation",
           handler: () => {
             window.location.href = "/multi-language-al-folio/fr-ca/cv/";
